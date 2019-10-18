64b35c938d87b7cb029587579baae3bf5da90aa6
xuliwen <1853959636@qq.com>
2019-10-18 11:18:51 +0800

    [modify][1.0.0] solve some import error

--- library/src/androidTest/java/com/xuliwen/zoom/ExampleInstrumentedTest.java
+++ library/src/androidTest/java/com/xuliwen/zoom/ExampleInstrumentedTest.java
@@ -2,13 +2,13 @@ package com.xuliwen.zoom;
 
 import android.content.Context;
 
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
